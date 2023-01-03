cdb2b0646db9e85ccff3ad54edd6793a225fdeec
ritesh <riteshkumar237@gmail.com>
2023-01-03 22:12:28 +0530

    created object and define

--- src/main/java/com/spring/api/SpringApiApplication.java
+++ src/main/java/com/spring/api/SpringApiApplication.java
@@ -1,5 +1,6 @@
 package com.spring.api;
 
+import com.spring.api.model.Employee;
 import com.spring.api.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -19,6 +20,18 @@ public class SpringApiApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
+
+		Employee employee = new Employee();
+		employee.setFirstName("Rakesh");
+		employee.setLastName("Kumar");
+		employee.setEmail("rakesh@email.com");
+		employeeRepository.save(employee);
+
+		Employee employee1 = new Employee();
+		employee1.setFirstName("Rajesh");
+		employee1.setLastName("Kumar");
+		employee1.setEmail("rajesh@email.com");
+		employeeRepository.save(employee1);
 
 	}
 }
