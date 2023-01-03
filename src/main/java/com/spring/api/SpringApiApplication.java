e061b1c59063c8a6b9e463796625302b2c17e8a9
ritesh <riteshkumar237@gmail.com>
2023-01-03 21:06:17 +0530

    Autowired field in class SpringApiApplication.java

--- src/main/java/com/spring/api/SpringApiApplication.java
+++ src/main/java/com/spring/api/SpringApiApplication.java
@@ -1,5 +1,7 @@
 package com.spring.api;
 
+import com.spring.api.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,5 +11,8 @@ public class SpringApiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringApiApplication.class, args);
 	}
+
+	@Autowired
+	private EmployeeRepository employeeRepository;
 
 }
