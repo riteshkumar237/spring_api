e97f64259a833b5e1157ab738e22acace5e8bb1d
ritesh <riteshkumar237@gmail.com>
2023-01-03 21:04:10 +0530

    created getAllEmployees() method

--- src/main/java/com/spring/api/controller/Controller.java
+++ src/main/java/com/spring/api/controller/Controller.java
@@ -1,13 +1,25 @@
 package com.spring.api.controller;
 
+import com.spring.api.model.Employee;
 import com.spring.api.repository.EmployeeRepository;
+import lombok.Getter;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
+@RequestMapping("/api/v1/employees")
 public class Controller {
 
     @Autowired
     private EmployeeRepository employeeRepository;
+
+    @GetMapping
+    public List<Employee> getAllEmployees(){
+        return employeeRepository.findAll();
+    }
 
 }
