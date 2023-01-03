221026bb383baab77bdad1995c08f213954c1c2f
ritesh <riteshkumar237@gmail.com>
2023-01-03 20:36:33 +0530

    created constructor with parameter and added ResponseStatus Annotation

--- src/main/java/com/spring/api/exception/ResourceNotFoundException.java
+++ src/main/java/com/spring/api/exception/ResourceNotFoundException.java
@@ -1,4 +1,12 @@
 package com.spring.api.exception;
 
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus
 public class ResourceNotFoundException extends RuntimeException{
+
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+
 }
