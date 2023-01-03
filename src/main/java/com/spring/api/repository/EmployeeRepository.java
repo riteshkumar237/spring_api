55e643a600260922ad9b721e4c7012f9090f7a4f
ritesh <riteshkumar237@gmail.com>
2023-01-03 20:29:13 +0530

    extended JpaRepository

--- src/main/java/com/spring/api/repository/EmployeeRepository.java
+++ src/main/java/com/spring/api/repository/EmployeeRepository.java
@@ -1,4 +1,7 @@
 package com.spring.api.repository;
 
-public interface EmployeeRepository {
+import com.spring.api.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
 }
