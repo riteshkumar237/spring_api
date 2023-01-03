df207236e8259aaf27fc7920798ff4a3f16de424
ritesh <riteshkumar237@gmail.com>
2023-01-03 20:43:12 +0530

    created field for Controller.java

--- src/main/java/com/spring/api/controller/Controller.java
+++ src/main/java/com/spring/api/controller/Controller.java
@@ -1,4 +1,13 @@
 package com.spring.api.controller;
 
+import com.spring.api.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class Controller {
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
 }
