936df28aa73fdba722ee62e848d662d9117bc61d
ritesh <riteshkumar237@gmail.com>
2023-01-03 21:08:05 +0530

    implemented CommandLineRunner and override rum() method

--- src/main/java/com/spring/api/SpringApiApplication.java
+++ src/main/java/com/spring/api/SpringApiApplication.java
@@ -2,11 +2,12 @@ package com.spring.api;
 
 import com.spring.api.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringApiApplication {
+public class SpringApiApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringApiApplication.class, args);
@@ -15,4 +16,9 @@ public class SpringApiApplication {
 	@Autowired
 	private EmployeeRepository employeeRepository;
 
+
+	@Override
+	public void run(String... args) throws Exception {
+
+	}
 }
